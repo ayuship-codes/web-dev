dffc084224fe62a611487f263a3dfa292bbb8439
Ayushi Prajapati <ayushi016750@gmail.com>
2026-05-02 19:01:11 +0530

    feat: add for loop and object iteration practice

---
+++ js/loops.js
@@ -0,0 +1,16 @@
+
+//for loop
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+//for in loop
+let obj={
+    name : "Rohan",
+    id : 1223
+}
+for (const key in obj) {  
+    const value = obj[key];
+    console.log(key, value)
+    
+}
