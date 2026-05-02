47d763e9ccbfe425b36d32a7e63c7f96a314b032
Ayushi Prajapati <ayushi016750@gmail.com>
2026-05-02 19:18:44 +0530

    feat:add basic functions in js

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+function greet(name){
+    console.log("Hello " + name + " !");
+}
+greet("Rohan");
+
+function printNum(n){
+    for(let i=1; i<=n;i++){
+        console.log(n)
+    }
+}
+printNum(5)
+
+function add(a,b){
+    return a+b
+}
+result=add(2,6)
+console.log(result)
