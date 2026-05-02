635970c0344bc13642474753f4c5b6864a66b37a
Ayushi Prajapati <ayushi016750@gmail.com>
2026-05-02 19:08:44 +0530

    feat: practice loops in js

--- js/loops.js
+++ js/loops.js
@@ -14,3 +14,23 @@ for (const key in obj) {
     console.log(key, value)
     
 }
+
+//for of loop
+let name = "Aaramya"
+for (const ch of name) {
+    console.log(ch)
+}
+
+//while loop
+let a = 1
+while(a<5){
+    console.log(a)
+    a++
+}
+
+//do while loop
+let b=10
+do{
+    console.log(b)
+    b++
+}while(b<5)
