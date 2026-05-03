1fb3bb26d3943b971cf9b6b0af8ff59187982412
Ayushi Prajapati <ayushi016750@gmail.com>
2026-05-03 22:25:34 +0530

    feat:learned and applied string in js

---
+++ js-intro/strings.js
@@ -0,0 +1,10 @@
+let a = "Namaste"
+let b = "Hello"
+console.log(a.length)
+console.log(b.length)
+console.log(a.concat(b))
+console.log(b.replace("He","00"))
+console.log(b.trim("ll"))
+
+console.log(a)
+console.log(b)
